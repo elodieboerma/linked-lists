3d8473c637923db10f8221d470d40ef18a6c9c2a
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2026-09-09 23:31:30 -0500

    correct watchFiles

--- webpack.config.js
+++ webpack.config.js
@@ -16,11 +16,11 @@ export default {
   },
   devtool: "eval-source-map",
   devServer: {
-    watchFiles: ["./src/template.html"],
+    watchFiles: ["./src/index.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
+      template: "./src/index.html",
     }),
   ],
   module: {
